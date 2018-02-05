27d36bbc267aa50dc640b105d58a38a4501cd271
artembert <artembert@gmail.com>
2018-02-05 23:30:53 +0300

    start making adaptive vesion

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
-'use strict';
+'use strict'
 
-(function() {
+var pageHeader = document.querySelector('.page-header');
 
-})();
+var startHomeWrapper = pageHeader.querySelector('.start-home__wrapper');
+
+startHomeWrapper.addEventListener("mouseover", function() {
+  pageHeader.classList.add('animation-iphone-rise');
+
+  // setTimeout(function() {
+  //   pageHeader.classList.remove('animation-iphone-rise');
+  // }, 3000);
+});
+
+startHomeWrapper.addEventListener("mouseout", function() {
+  pageHeader.classList.remove('animation-iphone-rise');
+});
